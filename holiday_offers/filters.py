bf6a3b63707dd185ce5427a62d43d7d2aa23b6ad
Paweł Kordowski <paw.mar.kor@gmail.com>
2018-07-29 10:18:18 +0200

    Filtering WIP

---
+++ holiday_offers/filters.py
@@ -0,0 +1,34 @@
+from decimal import Decimal
+from datetime import time
+from typing import Optional, List
+
+from xml_service_client import Offer
+
+
+def filter_offers(
+        offers: List[Offer],
+        earliest_departure_time: Optional[time] = None,
+        earliest_return_time: Optional[time] = None,
+        max_price: Optional[Decimal] = None,
+        min_price: Optional[Decimal] = None,
+        star_rating: Optional[bool] = False,
+):
+    filters = []
+    if max_price is not None:
+        filters.append(lambda offer: offer.sellprice <= max_price)
+    if min_price is not None:
+        filters.append(lambda offer: offer.sellprice >= min_price)
+    if star_rating:
+        filters.append(lambda offer: offer.starrating >= 3)
+    if earliest_departure_time:
+        filters.append(
+            lambda offer: offer.outbounddep.time() >= earliest_departure_time
+        )
+    if earliest_return_time:
+        filters.append(
+            lambda offer: offer.inbounddep.time() >= earliest_return_time
+        )
+    return [
+        offer for offer in offers
+        if all(map(lambda filter: filter(offer), filters))
+    ]
---
+++ holiday_offers/tests/test_filters.py
@@ -0,0 +1,87 @@
+from datetime import datetime, time
+from decimal import Decimal
+
+import pytest
+
+from xml_service_client import Offer
+from filters import filter_offers
+
+
+def generate_offer(
+        departure_time,
+        return_time,
+        price,
+        star_rating
+):
+    return Offer(
+        type="DP",
+        hotelsupplier="Hotel Beds",
+        flightsuppler="Norwegian Fly / Easyjet",
+        depaptcode="LGW",
+        depaptname="London, Gatwick",
+        arraptcode="TFS",
+        arraptname="Tenerife, Sur Int.(Reina Sofia",
+        outbounddep=datetime(
+            year=2018, month=8, day=15,
+            hour=departure_time.hour, minute=departure_time.minute
+        ),
+        outboundarr=datetime(
+            year=2018, month=8, day=16, hour=0, minute=25
+        ),
+        outboundfltnum="D86405",
+        inbounddep=datetime(
+            year=2018, month=8, day=22,
+            hour=return_time.hour, minute=return_time.minute
+        ),
+        inboundarr=datetime(
+            year=2018, month=8, day=22, hour=16, minute=55
+        ),
+        inboundfltnum="EZY8704",
+        duration=7,
+        hotelname="Globales Acuario",
+        resortname="Puerto De La Cruz",
+        roomtype="Enquire",
+        boardbasis="RO",
+        starrating=star_rating,
+        ourhtlid=2882,
+        brochurecode="HBED-119",
+        hotelnetprice=Decimal("83.00"),
+        flightnetprice=Decimal("360.00"),
+        sellprice=Decimal(price)
+    )
+
+
+@pytest.mark.parametrize(
+    'earliest_departure_time,earliest_return_time,'
+    'max_price,min_price,star_rating,expected_length',
+    [
+        (None, None, None, None, None, 1),
+        (None, None, None, 2, None, 0),
+    ]
+)
+def test_filter(
+        earliest_departure_time,
+        earliest_return_time,
+        max_price,
+        min_price,
+        star_rating,
+        expected_length
+):
+    offers = [
+        generate_offer(
+            departure_time=time(hour=12, minute=0),
+            return_time=time(hour=12, minute=0),
+            price=1,
+            star_rating=1,
+        )
+    ]
+    assert len(
+        filter_offers(
+            offers,
+            earliest_departure_time,
+            earliest_return_time,
+            max_price,
+            min_price,
+            star_rating
+        )
+    ) == expected_length
